1373d58ac017fe8397888b22836e9e0783e48804
Vishal Kumar <bt23mme087@students.vnit.ac.in>
2025-07-08 02:41:06 +0530

    Update mechanisms_ohmic.py

--- Mechanisms/mechanisms_ohmic.py
+++ Mechanisms/mechanisms_ohmic.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def is_ohmic(logV, logI, tol=0.15, r2_th=0.98):
+def is_ohmic(logV, logI, tol=0.2, r2_th=0.98):
     coeffs = np.polyfit(logV, logI, 1)
     slope = coeffs[0]
     pred = np.polyval(coeffs, logV)
